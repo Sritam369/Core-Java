8bda14f52f50ebde4013752255d2c30bb0504741
Sritam369 <chakrabarttysritam@gmail.com>
2025-10-29 12:41:30 +0530

    practice question

---
+++ SritamNIT/src/com/sri/practice/Person.java
@@ -0,0 +1,25 @@
+package com.sri.practice;
+
+
+class Employee{
+ 	 String name;
+ 	public Employee(String name) {
+  		this.name=name;  
+ 	}
+}
+public class Person {
+ 	public static void main(String[] Java) {
+  		Employee e = new Employee("JavaDeveloper");
+  		System.out.println("Emp Name : "+e.name);
+ 	}
+}
+
+
+
+
+
+
+
+
+
+
