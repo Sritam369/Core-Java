2b7b8ab895a792c68a1b3304bb897dc31196721c
Sritam369 <chakrabarttysritam@gmail.com>
2025-11-06 20:47:24 +0530

    practice question

---
+++ SritamNIT/src/com/ravi/practice/Test.java
@@ -0,0 +1,21 @@
+package com.ravi.practice;
+
+class Test 
+{
+    Test() 
+    { 
+    this(10); 
+    }
+    
+    Test(int x) 
+    { 
+    //this();
+    }
+    
+    public static void main(String[] args)
+    {
+        Test obj = new Test();
+    }
+}
+
+
