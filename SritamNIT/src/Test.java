a5c45c2d067fc038e37fe2f7f5403ae09ee6e756
Sritam369 <chakrabarttysritam@gmail.com>
2025-11-17 11:41:30 +0530

    STATIC BLOCK PRACTICE QUESTION

---
+++ SritamNIT/src/Test.java
@@ -0,0 +1,38 @@
+
+public class Test 
+{
+
+    static final int X;
+    static final int Y;
+
+    static 
+    {
+        X = 5;
+    }
+
+    static
+    {
+        Y = 10;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("X = " + X);
+        System.out.println("Y = " + Y);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
