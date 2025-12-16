7c1679eb0b3f7746d97077db2793c3d1473350b9
Sritam369 <chakrabarttysritam@gmail.com>
2025-12-16 09:38:23 +0530

    update on functional interface

--- SritamNIT/src/com/sri/consumer_functional_interface/ValueModifier.java
+++ SritamNIT/src/com/sri/consumer_functional_interface/ValueModifier.java
@@ -1,4 +1,4 @@
-package com.sri.lamda_expression;
+package com.sri.consumer_functional_interface;
 
 import java.util.Scanner;
 import java.util.function.Consumer;
--- SritamNIT/src/com/sri/predicate_functional_interface/LoanEligibility.java
+++ SritamNIT/src/com/sri/predicate_functional_interface/LoanEligibility.java
@@ -1,4 +1,4 @@
-package com.sri.lamda_expression;
+package com.sri.predicate_functional_interface;
 
 import java.util.Scanner;
 import java.util.function.Predicate;
